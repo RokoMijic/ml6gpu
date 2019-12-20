53987d17dea527250c5bb3fe7faec9677f33aac5
r <rmijic@gmail.com>
2019-12-20 23:11:25 +0100

    data augmentors

---
+++ main/data_augmentors.py
@@ -0,0 +1,30 @@
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# required for some preprocessing
+import scipy as sp
+import PIL
+
+
+
+
+
+def get_datagen_spec(transf_amnt = 0.0):
+
+    return  ImageDataGenerator(
+                                rotation_range=20*transf_amnt,
+                                width_shift_range=0.025*transf_amnt,
+                                height_shift_range=0.025*transf_amnt,
+                                shear_range=0.05*transf_amnt,
+                                zoom_range=0.2*transf_amnt,
+                                horizontal_flip=False,
+                                # channel_shift_range=0.05*transf_amnt,
+                                fill_mode='nearest'
+                              )
+
+
+if __name__ == "__main__":
+
+    pass
+
+    # save augmented images here 
