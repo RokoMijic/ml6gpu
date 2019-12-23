16f7bd82cf422c6ca6658f62a04c0284b72ea408
r <rmijic@gmail.com>
2019-12-23 01:56:43 +0100

    Made a bunch of models

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@
 from settings import IMG_DIM, COLOR_CHAN, NUM_CLASSES
 
 from tensorflow.keras import Sequential, regularizers
-from tensorflow.keras.layers import Dense , Flatten , Conv2D , MaxPool2D, BatchNormalization, Dropout, AveragePooling2D
+from tensorflow.keras.layers import Dense , Flatten , Conv2D , MaxPooling2D, BatchNormalization, Dropout, AveragePooling2D, Cropping2D
 
 
 
@@ -37,7 +37,7 @@ def get_simple_cnn(reg_para):
                                   ),
 
 
-                            MaxPool2D(pool_size=(3,3)),
+                            MaxPooling2D(pool_size=(3,3)),
 
                             Dropout(drop_p),
 
@@ -53,7 +53,7 @@ def get_simple_cnn(reg_para):
     return simpl_cnn
 
 
-def get_dense_nn(reg_para):
+def get_dense_nn(reg_para=0.5):
 
     reg = get_reg(reg_para)
     l = reg['l']
@@ -121,7 +121,7 @@ def get_cnn_v_lowres(reg_para=0.75):
 
 
 
-def get_cnn_lowres(reg_para=0.5):
+def get_cnn_lowres(reg_para=1.75):
 
     reg = get_reg(reg_para)
     l = reg['l']
@@ -149,7 +149,40 @@ def get_cnn_lowres(reg_para=0.5):
     return model
 
 
-def get_cnn_med_res(reg_para=3.75):
+
+def get_cnn_low_cropping(reg_para=1.5):
+
+    reg = get_reg(reg_para)
+    l = reg['l']
+    drop_p = reg['drop_p']
+
+    model = Sequential([
+
+                        Cropping2D(cropping=8, input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN)  ),
+
+                        Conv2D(filters=10, kernel_size=(5, 5), strides=3, padding='same', activation='relu', kernel_regularizer=regularizers.l1_l2(l),  ),
+
+                        Dropout(drop_p),
+
+                        Conv2D(filters=16, kernel_size=(5, 5), strides=3, padding='same', activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(drop_p),
+
+                        Flatten(),
+
+                        Dense(8, activation='relu', kernel_regularizer=regularizers.l2( l ) ),
+
+                        Dropout(drop_p),
+
+                        Dense(NUM_CLASSES, activation='softmax', kernel_regularizer=regularizers.l2( l))
+                     ])
+
+    return model
+
+
+
+
+def get_cnn_med_res(reg_para=2.75):
 
     l = get_reg(reg_para)['l']
     drop_p = get_reg(reg_para)['drop_p']
@@ -216,7 +249,7 @@ def get_cnn_better_res(reg_para=3.75):
     return model
 
 
-def get_small_lenet_cnn(reg_para):
+def get_small_lenet_crop_cnn(reg_para=1.95):
 
     reg = get_reg(reg_para)
 
@@ -224,27 +257,28 @@ def get_small_lenet_cnn(reg_para):
     drop_p = reg['drop_p']
 
     cnn = Sequential([
-                        Conv2D(filters=4, kernel_size=(3, 3), activation='relu', input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN), kernel_regularizer=regularizers.l2( l  )),
 
-                        Conv2D(filters=6, kernel_size=(5, 5), strides=3, padding='same', activation='relu', kernel_regularizer=regularizers.l2( l )),
+                        Cropping2D(cropping=8, input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN)),
 
-                        Dropout( drop_p ),
+                        Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN), kernel_regularizer=regularizers.l2( l  )),
 
-                        Conv2D(filters=8, kernel_size=(3, 3), activation='relu', kernel_regularizer=regularizers.l2( l )),
-
-                        Conv2D(filters=10, kernel_size=(5, 5), strides=3, padding='same', activation='relu', kernel_regularizer=regularizers.l2( l )),
+                        MaxPooling2D(pool_size=3, padding='valid'),
 
                         Dropout( drop_p ),
 
-                        Conv2D(filters=14, kernel_size=(3, 3), activation='relu', kernel_regularizer=regularizers.l2(l)),
+                        Conv2D(filters=16, kernel_size=(3, 3), activation='relu', kernel_regularizer=regularizers.l2( l )),
 
-                        Conv2D(filters=16, kernel_size=(5, 5), strides=3, padding='same', activation='relu',  kernel_regularizer=regularizers.l2(l)),
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=16, kernel_size=(3, 3), activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        MaxPooling2D(pool_size=3, padding='valid'),
 
                         Flatten(),
 
                         Dropout(  drop_p ),
 
-                        Dense(20, activation='relu', kernel_regularizer=regularizers.l2(l)),
+                        Dense(30, activation='relu', kernel_regularizer=regularizers.l2(l)),
 
                         Dropout(  drop_p ),
 
@@ -254,7 +288,174 @@ def get_small_lenet_cnn(reg_para):
     return cnn
 
 
-def get_lenet_cnn(reg_para=3.35):
+
+def get_med_lenet_crop_cnn(reg_para=1.95):
+
+    reg = get_reg(reg_para)
+
+    l = reg['l']
+    drop_p = reg['drop_p']
+
+    cnn = Sequential([
+
+                        Cropping2D(cropping=8, input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN)),
+
+                        Conv2D(filters=6, kernel_size=(3, 3), activation='relu', padding='same',  input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN), kernel_regularizer=regularizers.l2( l  )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2( l )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same',  kernel_regularizer=regularizers.l2(l)),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Flatten(),
+
+                        Dropout(  drop_p ),
+
+                        Dense(40, activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(  drop_p ),
+
+                        Dense(NUM_CLASSES, activation='softmax', kernel_regularizer=regularizers.l2( l ))
+                    ])
+    return cnn
+
+
+
+
+def get_bigish_lenet_crop_cnn(reg_para=2.25):
+
+    # This is the best model!
+
+    reg = get_reg(reg_para)
+
+    l = reg['l']
+    drop_p = reg['drop_p']
+
+    cnn = Sequential([
+
+                        Cropping2D(cropping=9, input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN)),
+
+                        Conv2D(filters=12, kernel_size=(3, 3), activation='relu', padding='same',  input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN), kernel_regularizer=regularizers.l2( l  )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=20, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2( l )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same',  kernel_regularizer=regularizers.l2(l)),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Flatten(),
+
+                        Dropout(  drop_p ),
+
+                        Dense(40, activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(  drop_p ),
+
+                        Dense(15, activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(drop_p),
+
+                        Dense(NUM_CLASSES, activation='softmax', kernel_regularizer=regularizers.l2(l))
+                    ])
+    return cnn
+
+
+
+def get_big_lenet_crop_cnn(reg_para=2.5):
+
+    reg = get_reg(reg_para)
+
+    l = reg['l']
+    drop_p = reg['drop_p']
+
+    cnn = Sequential([
+
+                        Cropping2D(cropping=9, input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN)),
+
+                        Conv2D(filters=10, kernel_size=(3, 3), activation='relu', padding='same',  input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN), kernel_regularizer=regularizers.l2( l  )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=20, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2( l )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=40, kernel_size=(3, 3), activation='relu', padding='same',  kernel_regularizer=regularizers.l2(l)),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Flatten(),
+
+                        Dropout(  drop_p ),
+
+                        Dense(30, activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(  drop_p ),
+
+                        Dense(15, activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(drop_p),
+
+                        Dense(NUM_CLASSES, activation='softmax', kernel_regularizer=regularizers.l2(l))
+                    ])
+    return cnn
+
+
+
+def get_huge_lenet_crop_cnn(reg_para=2.95):
+
+    reg = get_reg(reg_para)
+
+    l = reg['l']
+    drop_p = reg['drop_p']
+
+    cnn = Sequential([
+
+                        Cropping2D(cropping=9, input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN)),
+
+                        Conv2D(filters=20, kernel_size=(3, 3), activation='relu', padding='same',  input_shape=(IMG_DIM, IMG_DIM, COLOR_CHAN), kernel_regularizer=regularizers.l2( l  )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=35, kernel_size=(3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2( l )),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Dropout( drop_p ),
+
+                        Conv2D(filters=80, kernel_size=(3, 3), activation='relu', padding='same',  kernel_regularizer=regularizers.l2(l)),
+                        MaxPooling2D(pool_size=3, padding='same'),
+
+                        Flatten(),
+
+                        Dropout(  drop_p ),
+
+                        Dense(65, activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(  drop_p ),
+
+                        Dense(45, activation='relu', kernel_regularizer=regularizers.l2(l)),
+
+                        Dropout(drop_p),
+
+                        Dense(NUM_CLASSES, activation='softmax', kernel_regularizer=regularizers.l2(l))
+                    ])
+    return cnn
+
+
+def get_lenet_cnn(reg_para=2.5):
 
     l = get_reg(reg_para)['l']
     drop_p = get_reg(reg_para)['drop_p']
@@ -331,5 +532,6 @@ def get_big_lenet_cnn(reg_para=2.95):
 
 if __name__ == "__main__":
 
-    model = get_cnn_v_lowres(4.0)
+    model = get_bigish_lenet_crop_cnn()
     print(model.summary())
+
